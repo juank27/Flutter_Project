bc3f85951dd83279e8f78509becd35563c3f5f4f
juank27 <juankruiz27@gmail.com>
2022-03-18 11:28:13 -0500

    generando campos de ingreso en login

--- my_app/lib/View/Login.dart
+++ my_app/lib/View/Login.dart
@@ -23,6 +23,38 @@ class LoginApp extends State<Login> {
                 child: Image.asset('image/img.png'),
               )),
             ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'User',
+                  hintText: 'Digite el usuario',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Pass',
+                  hintText: 'Digite contraseña',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(minimumSize: Size(500, 50)),
+                onPressed: () {
+                  print('Ingresar');
+                },
+                child: Text('Ingresar'),
+              ),
+            )
           ]),
         ));
   }
