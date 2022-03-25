c322b2d02634b326df72d6206e086732400209dc
juank27 <juankruiz27@gmail.com>
2022-03-25 11:02:47 -0500

    creacion de la plantilla de registro

--- my_app/lib/View/UserRegister.dart
+++ my_app/lib/View/UserRegister.dart
@@ -6,13 +6,125 @@ class UserRegister extends StatefulWidget {
 }
 
 class UserRegisterApp extends State<UserRegister> {
+  var currentSatate;
+  DateTime selectedDate = DateTime.now();
+
+  void callDataPicker() async {
+    // var selectedDate =await getDatePickerWidget();
+    setState(() {
+      currentSatate = selectedDate;
+    });
+  }
+//Future <DateTime?> getDatePickerWidget(){
+  //return showDatePicker(
+
+  // )
+//}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Registro de Usuario'),
+        title: Text('Registro de usuario'),
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Center(
+                child: Container(
+                  width: 500,
+                  height: 70,
+                  child: Image.asset('image/img.png'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                autofocus: true,
+                keyboardType: TextInputType.name,
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Nombres',
+                  hintText: 'Digite nombres y apellido',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                autofocus: true,
+                keyboardType: TextInputType.name,
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Identificación ',
+                  hintText: 'Digite nombres y apellido',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Identificación',
+                  hintText: 'Digite si identifiación',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                autofocus: true,
+                keyboardType: TextInputType.emailAddress,
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Correo',
+                  hintText: 'Digite su correo',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15, top: 10),
+              child: Stack(
+                alignment: const Alignment(1.0, 1.0),
+                children: [
+                  new TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      labelText: 'Fecha de nacimiento',
+                      hintText: 'Digite su fecha de nacimiento',
+                    ),
+                  ),
+                  new FlatButton(
+                      onPressed: () {},
+                      child: new Icon(Icons.date_range_outlined))
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'View/Login.dart';
+import 'View/UserRegister.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -51,7 +52,19 @@ class HomeStart extends State<Home> {
                 },
                 child: const Text('Ingresar'),
               ),
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, top: 10, right: 10, bottom: 0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(minimumSize: Size(400, 50)),
+                onPressed: () {
+                  print('Registrar Usuario');
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => UserRegister()));
+                },
+                child: Text('Registrar Usuario'),
+              ),
+            ),
           ],
         ),
       ),
